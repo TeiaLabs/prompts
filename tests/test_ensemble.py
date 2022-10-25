de86b24747c9a852e37da79a18c592fd3b5b3aa5
jwehrmann <jonataswehrmann@gmail.com>
2022-10-25 15:42:04 -0300

    Add tests for ensembling from files

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -1,5 +1,6 @@
 import pytest 
 from prompts.ensemble import PromptEnsemble
+from prompts import DynamicPrompt
 
 
 def test_ensemble():
@@ -8,7 +9,8 @@ def test_ensemble():
     
     prompt = PromptEnsemble(templates, vars)
 
-    prompted_list = prompt.build(label='dog')
+    prompted_list = prompt.build(label='dog')    
+
     assert len(prompted_list) == 3
     assert prompted_list[0] == 'dog'
     assert prompted_list[1] == 'a photo of dog'
@@ -25,3 +27,27 @@ def test_invalid_ensemble_template():
    # expect exception
     with pytest.raises(ValueError):
         _ = PromptEnsemble(templates, vars)
+
+
+def test_prompt_ensemble_from_file():
+    prompts = []
+    for i in range(3):
+        prompt_file = 'samples/sample.prompt'
+        prompt = DynamicPrompt.from_file(prompt_file)
+        prompts.append(prompt)
+    
+    prompt_ens = PromptEnsemble(prompts)
+    prompt_str = prompt_ens.build(
+        input_sentence='lets go'
+    )
+    assert len(prompt_str) == 3
+
+    prompt_ens_ff = PromptEnsemble.from_paths(
+        [prompt_file, prompt_file, prompt_file]
+    )
+
+    prompt_ff = prompt_ens_ff.build(
+        input_sentence='lets go'
+    )
+
+    assert prompt_ff == prompt_str
