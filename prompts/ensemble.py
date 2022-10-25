3323157ce2d6d9cba1b90981c7f6f62ed6398126
jwehrmann <jonataswehrmann@gmail.com>
2022-10-25 15:19:31 -0300

    Add prompt ensemble

---
+++ prompts/ensemble.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from .prompt_builder import DynamicPrompt
+
+
+class PromptEnsemble:
+
+    def __init__(self, templates: list[str], expected_vars: list[str]):
+        """
+        Args:
+            templates: templates with placeholder variable names
+            expected_vars: variables expected in all templates
+
+        Return:
+            A prompt ensemble object.
+
+        Examples:
+        ```
+            templates = ["a photo of a <class>", "picture of <class>"]
+            expected_vars = ["class"]
+            prompt = PromptEnsemble(templates, expected_vars)
+        ```
+        """
+        self.prompts = []
+        for template in templates:
+            self.prompts.append(DynamicPrompt(template, expected_vars))
+
+    def build(self, **kwargs):
+        filled_prompts = []
+        for prompt in self.prompts:
+            filled_prompts.append(prompt.build(**kwargs))
+        return filled_prompts
